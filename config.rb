c003c6f399d7cb702aec2988780bfc190eebf17c
Matt Aimonetti <mattaimonetti@gmail.com>
2012-04-17 12:24:02 +0200

    updated

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/mattetti/"
-http_images_path = "/mattetti/images"
-http_fonts_path = "/mattetti/fonts"
-css_dir = "public/mattetti/stylesheets"
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
