f84dc1983088f0bc3c78727dfe930da967bc94f8
Matt Aimonetti <mattaimonetti@gmail.com>
2012-04-20 01:32:15 +0200

    Site updated at 2012-04-19 23:32:15 UTC

--- javascripts/slash.js
+++ javascripts/slash.js
@@ -2,15 +2,15 @@
 	/* external.js */
 	var host = location.host;
 
-	$('a').on('click', function(e){
-		var href = $(this).attr('href'),
-			link = href.replace(/(https?:\/\/)(.*)\/(.*)/, '$2');
+	// $('a').on('click', function(e){
+	//   var href = $(this).attr('href'),
+	//     link = href.replace(/(https?:\/\/)(.*)\/(.*)/, '$2');
 
-		if (href.match('https?') && link != host){
-			window.open(href);
-			e.preventDefault();
-		}
-	});
+	//   if (href.match('https?') && link != host){
+	//     window.open(href);
+	//     e.preventDefault();
+	//   }
+	// });
 
 	/* navigation.js */
 	var appends = '<option>Menu</option>';
