e09c7b6a11b69c5fb73f1d992fafa0eebbbbf0f4
Matt Aimonetti <mattaimonetti@gmail.com>
2012-04-25 16:45:08 +0200

    Site updated at 2012-04-25 14:45:08 UTC

--- javascripts/slash.js
+++ javascripts/slash.js
@@ -33,12 +33,13 @@
 		var _i = i;
 		$(this).find('img').each(function(){
 			var alt = $(this).attr('alt');
-
-			if (alt == '' || typeof alt == 'undefined'){
-				$(this).wrap('<a href="'+$(this).attr('src')+'" class="fancybox" rel="gallery'+_i+'" />');
-			} else {
-				$(this).after('<span class="caption">'+alt+'</span>').wrap('<a href="'+$(this).attr('src')+'" class="fancybox" title="'+alt+'" rel="gallery'+_i+'" />');
-			}
+      if !($(this).hasClass('no-caption')){
+        if (alt == '' || typeof alt == 'undefined'){
+          $(this).wrap('<a href="'+$(this).attr('src')+'" class="fancybox" rel="gallery'+_i+'" />');
+        } else {
+          $(this).after('<span class="caption">'+alt+'</span>').wrap('<a href="'+$(this).attr('src')+'" class="fancybox" title="'+alt+'" rel="gallery'+_i+'" />');
+        }
+      }
 		});
 	});
 	$('.fancybox').fancybox();
